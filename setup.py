802f417c19303b57db7a28cff299d716885f70d5
Seth Howes <sethshowes@gmail.com>
2023-05-15 12:31:48 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A reference toxicology benchmark for small-molecule few-shot representation learning',
+    author='Seth Howes',
+    license='MIT',
+)
