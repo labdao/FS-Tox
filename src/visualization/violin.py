e4c8f2549176f2c4373019f52840aee2ea0a6b17
Seth Howes <sethshowes@gmail.com>
2023-06-01 16:13:23 +0200

    created script for violin plot

--- src/visualization/violin.py
+++ src/visualization/violin.py
@@ -1,29 +1,46 @@
-import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-import click
+import pandas as pd
 import duckdb
+import click
+import logging
+
 
 @click.command()
 @click.argument("input_filepath", type=click.Path(exists=True))
 @click.option("-f", "--features", multiple=True)
-def main(input_filepath, features):
+@click.option("-d", "--dataset")
+def main(input_filepath, features, dataset):
     con = duckdb.connect()
 
-    # Create a glob pattern to match the features
-    features_pattern = "|".join(features)
+    logging.info(f"Loading evaluation metrics from {input_filepath}...")
+
+    # Create filepath pattern for each feature
+    score_filepaths = [f"{input_filepath}/score_*{dataset}_{feature}.parquet" for feature in features]
+
+    # Convert list to string so it can be incorporated into the SQL query
+    score_filepaths_as_str = str(score_filepaths)
 
     # Load scoring data from parquet files
     pred_df = con.execute(
         f"""
             SELECT feature, auc_roc
-            FROM read_parquet('{input_filepath}/score_*{{{features_pattern}}}.parquet'), filename=true
+            FROM read_parquet({score_filepaths_as_str})
             """
     ).fetchdf()
 
-    sns.violinplot(x=pred_df["feature"], y=pred_df["auc_roc"])
+    logging.info("Creating swarmplot...")
+    
+    # Create violin plot
+    sns.violinplot(x=pred_df["feature"], y=pred_df["auc_roc"], hue=pred_df["feature"], palette='Set2')
+    plt.title(f"Violin plots of AUC-ROC scores for assays in {dataset}")
+
+    plt.show()
 
 
 if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(message)s"
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
     main()
