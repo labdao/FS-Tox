141d8c7e6fc50f247716c68367e04b3795334ee6
Seth Howes <sethshowes@gmail.com>
2023-06-19 12:43:18 +0100

    minor changes to evaluation script

--- src/models/evaluation.py
+++ src/models/evaluation.py
@@ -27,22 +27,31 @@ def main(input_filepath, output_filepath, assay, dataset):
         pred_filenames = [f for f in os.listdir(input_filepath) if assay in f]
 
     if dataset:
+<<<<<<< Updated upstream
         pred_filenames = [f for f in os.listdir(input_filepath) if any(d in f for d in dataset)]
+=======
+        pred_filenames = [
+            f for f in os.listdir(input_filepath) if any(d in f for d in dataset)
+        ]
+>>>>>>> Stashed changes
 
     # Create a list to store the metric dictionaries
     feature_performance = []
 
     for pred_filename in pred_filenames:
-        
         # Create empty dictionary to store metrics
         metrics_dict = {}
 
         # Load the predictions
         df = pd.read_parquet(os.path.join(input_filepath, pred_filename))
+<<<<<<< Updated upstream
         
+=======
+
+>>>>>>> Stashed changes
         # Get embeding name from filename
         filename_without_extension, _ = os.path.splitext(pred_filename)
-        feature_name = re.search(r'[^_]*$', filename_without_extension).group()
+        feature_name = re.search(r"[^_]*$", filename_without_extension).group()
 
         # Get the true labels and the predicted labels
         y_true = df["ground_truth"]
@@ -58,17 +67,25 @@ def main(input_filepath, output_filepath, assay, dataset):
         try:
             y_score = df["preds_proba"]
             metrics_dict["auc_roc"] = roc_auc_score(y_true, y_score)
+<<<<<<< Updated upstream
             metrics_dict["auc_pr"] = average_precision_score(y_true, y_score) # calculate AUC-PR
+=======
+            metrics_dict["auc_pr"] = average_precision_score(
+                y_true, y_score
+            )  # calculate AUC-PR
+>>>>>>> Stashed changes
         except ValueError as e:
-            logger.warn("Cannot compute ROC AUC score because ground truth data contains only one class. Outputting NaN for ROC AUC")
-            metrics_dict["auc_roc"] = float('nan')
-        
+            logger.warn(
+                "Cannot compute ROC AUC score because ground truth data contains only one class. Outputting NaN for ROC AUC"
+            )
+            metrics_dict["auc_roc"] = float("nan")
+
         # Convert the dictionary to a dataframe
         metrics_df = pd.DataFrame(metrics_dict, index=[0])
-    
+
         # Get the assay name from the filename
         assay_feature_name = pred_filename.replace("preds_", "").split(".")[0]
-        
+
         # Save inidividual assay metrics to a parquet file
         metrics_df.to_parquet(f"{output_filepath}/score_{assay_feature_name}.parquet")
 
